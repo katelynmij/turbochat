199db2cce3399b974471cadb1b6c511c1a913a04
Katelyn Mijares <katelynmijares@gmail.com>
2025-10-26 01:02:22 -0500

    creating realtime chat rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_25_055902) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_26_051945) do
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.boolean "is_private", default: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
